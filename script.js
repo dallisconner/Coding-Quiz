4f4917aa15168dac37b4fb58c0a4f9d4ddd8e6f8
8bitk9 <67807672+dallisconner@users.noreply.github.com>
2020-08-10 23:15:12 -0400

    Quiz ending now leads to highscore page

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var btn1 = document.querySelector("#button1");
 var btn2 = document.querySelector("#button2");
 var btn3 = document.querySelector("#button3");
 var btn4 = document.querySelector("#button4");
+var buttons = document.querySelector(".qButton");
 var cbFlag = true;
 var qTitle = document.querySelector(".qTitle");
 var qText = document.querySelector(".qText");
@@ -21,7 +22,7 @@ btn1.addEventListener("click", function()
         for(var i=0;i<3;i++)
         {
         var newBtn = document.createElement("button");
-        newBtn.setAttribute("class","btn-primary btn-lg");
+        newBtn.setAttribute("class","btn-primary btn-lg qButton");
         newBtn.setAttribute("id","button"+(i+2));
         newBtn.textContent = "New Button";
         cardBody.append(newBtn);
@@ -51,7 +52,17 @@ btn1.addEventListener("click", function()
         question4();
         qOrder++;
     }
+    else if (qOrder>3)
+    {
+        window.location.href = "highscore.html";
+    }
+
 });
+
+buttons.addEventListener("click", function()
+{
+    
+})
 
 //Create questions
 function question1()
@@ -108,92 +119,7 @@ function startTimer()
         else
         {
             time.textContent = "Time's Up!";
-            return;
+            window.location.href = "highscore.html";
         }
     },1000);
 }
-
-//Store high scores
-var todoInput = document.querySelector("#todo-text");
-var todoForm = document.querySelector("#todo-form");
-var todoList = document.querySelector("#todo-list");
-var todoCountSpan = document.querySelector("#todo-count");
-
-var todos = [];
-
-init();
-
-function renderTodos() {
-  // Clear todoList element and update todoCountSpan
-  todoList.innerHTML = "";
-  todoCountSpan.textContent = todos.length;
-
-  // Render a new li for each todo
-  for (var i = 0; i < todos.length; i++) {
-    var todo = todos[i];
-
-    var li = document.createElement("li");
-    li.textContent = todo;
-    li.setAttribute("data-index", i);
-
-    var button = document.createElement("button");
-    button.textContent = "Complete";
-
-    li.appendChild(button);
-    todoList.appendChild(li);
-  }
-}
-
-function init() {
-  // Write code here to check if there are todos in localStorage
-  // If so, parse the value from localStorage and assign it to the todos variable
-
-  if(localStorage.getItem("todos") != null)
-  {
-    todos = JSON.parse(localStorage.getItem("todos"));
-  }
-
-  // Render todos to the DOM
-  renderTodos();
-}
-
-function storeTodos() {
-  // Add code here to stringify the todos array and save it to the "todos" key in localStorage
-  localStorage.setItem("todos",JSON.stringify(todos));
-}
-
-// When form is submitted...
-todoForm.addEventListener("submit", function(event) {
-  event.preventDefault();
-
-  var todoText = todoInput.value.trim();
-
-  // Return from function early if submitted todoText is blank
-  if (todoText === "") {
-    return;
-  }
-
-  // Add new todoText to todos array, clear the input
-  todos.push(todoText);
-  todoInput.value = "";
-
-  // Store updated todos in localStorage, re-render the list
-  storeTodos();
-  renderTodos();
-});
-
-// When a element inside of the todoList is clicked...
-todoList.addEventListener("click", function(event) {
-  var element = event.target;
-
-  // If that element is a button...
-  if (element.matches("button") === true) {
-    // Get its data-index value and remove the todo element from the list
-    var index = element.parentElement.getAttribute("data-index");
-    todos.splice(index, 1);
-
-    // Store updated todos in localStorage, re-render the list
-    storeTodos();
-    renderTodos();
-  }
-});
